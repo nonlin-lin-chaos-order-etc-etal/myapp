c7e533eb51aba764b09bdec8f177a9b68020c925
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2023-05-30 04:32:54 +0800

    Add title and description

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+// https://github.com/easy-wallet/easy-wallet
+
 import React from 'react';
-import {View, Button, Alert} from 'react-native';
+import {View, Text, Button, Alert, StyleSheet} from 'react-native';
 
 const onPressCreateWallet  = () => {
   Alert.alert("todo onPressCreateWallet"); //TODO
@@ -9,27 +11,59 @@ const onPressImportWallet  = () => {
 }
 const YourApp = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        padding: 10
-      }}>
-      <Button
-        onPress={onPressCreateWallet}
-        title="Create Wallet"
-        accessibilityLabel="Press this button to create a new wallet."
-      />
-      <View style={{padding:5}}></View>
-      <Button
-        onPress={onPressImportWallet}
-        title="Import Wallet"
-        accessibilityLabel="Press this button to import an existing wallet &mdash; an Easy Wallet's one or an external one."
-      />
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+        }}>
+        <Text style={styles.titleText}>Easy Wallet</Text>
+      </View>
+      <View
+        style={{
+          flex: 0.33,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Text style={styles.baseText}>An open source multiplatform crypto wallet.</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+        }}>
+        <Button
+          onPress={onPressCreateWallet}
+          title="Create Wallet"
+          accessibilityLabel="Press this button to create a new wallet."
+        />
+        <View style={{padding:5}}></View>
+        <Button
+          onPress={onPressImportWallet}
+          title="Import Wallet"
+          accessibilityLabel="Press this button to import an existing wallet &mdash; an Easy Wallet's one or an external one."
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  baseText: {
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default YourApp;
